eefa30879e718fea8ce6f638644771c154f60a6b
Thanakrit_ch <thanakritcharoenchinnapat@gmail.com>
2025-02-16 02:13:39 +0700

    Edit File Right_Section index.tsx and Add Extensions

--- app/Section/Right_Section/index.tsx
+++ app/Section/Right_Section/index.tsx
@@ -7,7 +7,6 @@ const RightSection = () => {
         <div className="grid gap-y-40 px-5">
             <div id="profile"><Profile /></div>
             <div id="project"><Project /></div>
-
             <div className="mb-96">Experience</div>
             <div className="mb-96">Certificate</div>
         </div>
