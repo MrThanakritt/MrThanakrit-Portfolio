e5ca6ca9439387b9c3f9d701452e22532b33d7e2
Thanakrit_ch <thanakritcharoenchinnapat@gmail.com>
2025-02-24 23:50:59 +0700

    plese finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import AllSection from "./Section/All_Section";
-import { title } from "process";
-import { image } from "framer-motion/client";
 
 export const metadata = {
   title: "MrThanakritt",
-  image: "/Thanakrit01.jpg"
 }
 
 export default function Portfolio() {
