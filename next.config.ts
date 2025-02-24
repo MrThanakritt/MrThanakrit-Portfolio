fe8ae444e2198527e75304652baeb5e072352628
Thanakrit_ch <thanakritcharoenchinnapat@gmail.com>
2025-02-25 01:25:42 +0700

    fix please

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
+import type { NextConfig } from "next";
+
 const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig = {
-  output: "export",
+const nextConfig: NextConfig = {
   assetPrefix: isProd ? "/MrThanakritt-Portfolio/" : "",
   images: {
     unoptimized: true,
   },
-} satisfies import("next").NextConfig; // ใช้ satisfies ให้ TypeScript ตรวจสอบโครงสร้าง
+};
 
 export default nextConfig;
