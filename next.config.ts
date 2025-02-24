b26efe0e4210e1d85616384a6576685734d0164c
Thanakrit_ch <thanakritcharoenchinnapat@gmail.com>
2025-02-24 21:59:10 +0700

    finishfinish

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export', // บังคับให้ Next.js สร้าง Static HTML
+  assetPrefix: isProd ? '/MrThanakritt-Portfolio/' : '', // กำหนด Path ให้ตรงกับ GitHub Pages
+  images: {
+    unoptimized: true, // ปิด Image Optimization (เพราะใช้ Static Export)
+  },
 };
 
 export default nextConfig;
